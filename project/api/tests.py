3ab058bac69d2d62e8ea9ca47290bb02a601bf39
whirish <lpoflynn@protonmail.ch>
2018-12-24 12:59:27 -0800

    Add tests

--- project/api/tests.py
+++ project/api/tests.py
@@ -20,8 +20,17 @@ along with this program.  If not, see <https://www.gnu.org/licenses/>.
 from django.core.exceptions import ValidationError
 from django.contrib.gis.geos import Point, Polygon
 from django.test import TestCase
+from django.urls import reverse
+from rest_framework import status
+from rest_framework.test import APITestCase
 
-from .factories import TerritorialEntityFactory, AtomicPolygonFactory
+from .factories import (
+    TerritorialEntityFactory,
+    AtomicPolygonFactory,
+    PoliticalRelationFactory,
+    CachedDataFactory,
+    SpacetimeVolumeFactory,
+)
 from .models import (
     PoliticalRelation,
     TerritorialEntity,
@@ -229,5 +238,337 @@ class ModelTest(TestCase):
             event_type=CachedData.BATTLE,
         )
 
-        self.assertTrue(hastings.rank > 0, hastings.date)
+        self.assertTrue(hastings.rank >= 0)
+        self.assertEqual(hastings.date, "0001-01-01")
         self.assertEqual(CachedData.objects.count(), 1)
+
+
+class APITest(APITestCase):
+    """
+    Tests operations through the API
+    """
+
+    @classmethod
+    def setUpTestData(cls):
+        """
+        Create basic model instances
+        """
+
+        # TerritorialEntities
+        cls.european_union = TerritorialEntityFactory(
+            wikidata_id=10, color=1, admin_level=1
+        )
+        cls.nato = TerritorialEntityFactory(wikidata_id=11, color=1, admin_level=1)
+
+        cls.germany = TerritorialEntityFactory(wikidata_id=20, color=1, admin_level=2)
+        cls.france = TerritorialEntityFactory(wikidata_id=21, color=1, admin_level=2)
+        cls.spain = TerritorialEntityFactory(wikidata_id=22, color=1, admin_level=2)
+        cls.italy = TerritorialEntityFactory(wikidata_id=23, color=1, admin_level=2)
+        cls.british_empire = TerritorialEntityFactory(
+            wikidata_id=24, color=1, admin_level=2
+        )
+        cls.british_hk = TerritorialEntityFactory(
+            wikidata_id=25, color=1, admin_level=2
+        )
+
+        cls.alsace = TerritorialEntityFactory(wikidata_id=30, color=1, admin_level=3)
+        cls.lorraine = TerritorialEntityFactory(wikidata_id=31, color=1, admin_level=3)
+
+        # AtomicPolygons
+        cls.alsace_geom = AtomicPolygonFactory(
+            name="Alsace", geom=Polygon(((1, 1), (1, 2), (2, 2), (1, 1)))
+        )
+
+        # PoliticalRelations
+        cls.EU_germany = PoliticalRelationFactory(
+            parent=cls.european_union,
+            child=cls.germany,
+            start_date="0001-01-01",
+            end_date="0002-01-01",
+            control_type=PoliticalRelation.INDIRECT,
+        )
+
+        # CachedData
+        cls.hastings = CachedDataFactory(
+            wikidata_id=1,
+            location=Point(0, 0),
+            date="0001-01-01",
+            event_type=CachedData.BATTLE,
+        )
+
+        # SpacetimeVolumes
+        cls.alsace_stv = SpacetimeVolumeFactory(
+            start_date="0001-01-01",
+            end_date="0002-01-01",
+            entity=cls.france,
+            references=["ref"],
+        )
+        cls.alsace_stv.territory.add(cls.alsace_geom)
+
+    def test_api_can_create_te(self):
+        """
+        Ensure we can create TerritorialEntities
+        """
+
+        url = reverse("territorialentity-list")
+        data = {"wikidata_id": 9, "color": "#fff", "admin_level": 4}
+        response = self.client.post(url, data, format="json")
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(TerritorialEntity.objects.count(), 11)
+        self.assertEqual(TerritorialEntity.objects.get(pk=9).admin_level, 4)
+
+    def test_api_can_update_te(self):
+        """
+        Ensure we can update TerritorialEntities
+        """
+
+        url = reverse("territorialentity-detail", args=[self.european_union.pk])
+        data = {"wikidata_id": 10, "color": "#fff", "admin_level": 5}
+        response = self.client.put(url, data, format="json")
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data["admin_level"], 5)
+
+    def test_api_can_query_tes(self):
+        """
+        Ensure we can query for all TerritorialEntities
+        """
+
+        url = reverse("territorialentity-list")
+        response = self.client.get(url, format="json")
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data[0]["wikidata_id"], self.european_union.pk)
+
+    def test_api_can_query_te(self):
+        """
+        Ensure we can query for individual TerritorialEntities
+        """
+
+        url = reverse("territorialentity-detail", args=[self.european_union.pk])
+        response = self.client.get(url, format="json")
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data["admin_level"], 1)
+
+    def test_api_can_create_pr(self):
+        """
+        Ensure we can create PoliticalRelations
+        """
+
+        url = reverse("politicalrelation-list")
+        data = {
+            "start_date": "0001-01-01",
+            "end_date": "0002-01-01",
+            "parent": self.european_union.pk,
+            "child": self.france.pk,
+            "control_type": PoliticalRelation.GROUP,
+        }
+        response = self.client.post(url, data, format="json")
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(PoliticalRelation.objects.count(), 2)
+        self.assertEqual(
+            PoliticalRelation.objects.last().control_type, PoliticalRelation.GROUP
+        )
+
+    def test_api_can_update_pr(self):
+        """
+        Ensure we can update PoliticalRelations
+        """
+
+        url = reverse("politicalrelation-detail", args=[self.EU_germany.pk])
+        data = {
+            "start_date": "0001-01-01",
+            "end_date": "0002-01-01",
+            "parent": self.european_union.pk,
+            "child": self.france.pk,
+            "control_type": PoliticalRelation.GROUP,
+        }
+        response = self.client.put(url, data, format="json")
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data["control_type"], PoliticalRelation.GROUP)
+
+    def test_api_can_query_prs(self):
+        """
+        Ensure we can query for all PoliticalRelations
+        """
+
+        url = reverse("politicalrelation-list")
+        response = self.client.get(url, format="json")
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data[0]["control_type"], PoliticalRelation.INDIRECT)
+
+    def test_api_can_query_pr(self):
+        """
+        Ensure we can query for individual PoliticalRelations
+        """
+
+        url = reverse("politicalrelation-detail", args=[self.EU_germany.pk])
+        response = self.client.get(url, format="json")
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data["control_type"], PoliticalRelation.INDIRECT)
+
+    def test_api_can_create_cd(self):
+        """
+        Ensure we can create CachedData
+        """
+
+        url = reverse("cacheddata-list")
+        data = {
+            "wikidata_id": 2,
+            "location": "Point(0 1)",
+            "date": "0001-01-01",
+            "event_type": CachedData.DOCUMENT,
+        }
+        response = self.client.post(url, data, format="json")
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(CachedData.objects.count(), 2)
+        self.assertEqual(CachedData.objects.last().event_type, CachedData.DOCUMENT)
+
+    def test_api_can_create_cd_othertype(self):
+        """
+        Ensure we can create CachedData with an event_type not in the choices
+        """
+
+        url = reverse("cacheddata-list")
+        data = {
+            "wikidata_id": 2,
+            "location": "Point(0 1)",
+            "date": "0001-01-01",
+            "event_type": 555,
+        }
+        response = self.client.post(url, data, format="json")
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(CachedData.objects.count(), 2)
+        self.assertEqual(CachedData.objects.last().event_type, 555)
+
+    def test_api_can_update_cd(self):
+        """
+        Ensure we can update CachedData
+        """
+
+        url = reverse("cacheddata-detail", args=[self.hastings.pk])
+        data = {
+            "wikidata_id": 1,
+            "location": "Point(0 0)",
+            "date": "0001-01-01",
+            "event_type": CachedData.DOCUMENT,
+        }
+        response = self.client.put(url, data, format="json")
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data["event_type"], CachedData.DOCUMENT)
+
+    def test_api_can_query_cds(self):
+        """
+        Ensure we can query for all CachedDatas
+        """
+
+        url = reverse("cacheddata-list")
+        response = self.client.get(url, format="json")
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data[0]["event_type"], CachedData.BATTLE)
+
+    def test_api_can_query_cd(self):
+        """
+        Ensure we can query for individual CachedDatas
+        """
+
+        url = reverse("cacheddata-detail", args=[self.hastings.pk])
+        response = self.client.get(url, format="json")
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data["event_type"], CachedData.BATTLE)
+
+    def test_api_can_create_ap(self):
+        """
+        Ensure we can create AtomicPolygons
+        """
+
+        url = reverse("atomicpolygon-list")
+        data = {"name": "Lorraine", "geom": "POLYGON((3 3, 3 4, 4 4, 3 3))"}
+        response = self.client.post(url, data, format="json")
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(AtomicPolygon.objects.count(), 2)
+        self.assertEqual(AtomicPolygon.objects.last().name, "Lorraine")
+
+    def test_api_can_update_ap(self):
+        """
+        Ensure we can update AtomicPolygon
+        """
+
+        url = reverse("atomicpolygon-detail", args=[self.alsace_geom.pk])
+        data = {"name": "Lorraine", "geom": "POLYGON((3 3, 3 4, 4 4, 3 3))"}
+        response = self.client.put(url, data, format="json")
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data["name"], "Lorraine")
+
+    def test_api_can_query_aps(self):
+        """
+        Ensure we can query for all AtomicPolygons
+        """
+
+        url = reverse("atomicpolygon-list")
+        response = self.client.get(url, format="json")
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data[0]["name"], "Alsace")
+
+    def test_api_can_query_ap(self):
+        """
+        Ensure we can query for individual AtomicPolygons
+        """
+
+        url = reverse("atomicpolygon-detail", args=[self.alsace_geom.pk])
+        response = self.client.get(url, format="json")
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data["name"], "Alsace")
+
+    def test_api_can_create_stv(self):
+        """
+        Ensure we can create SpacetimeVolumes
+        """
+
+        url = reverse("spacetimevolume-list")
+        data = {
+            "start_date": "0001-01-01",
+            "end_date": "0002-01-01",
+            "entity": self.germany.pk,
+            "references": ["ref"],
+            "territory": [self.alsace_geom.pk],
+        }
+        response = self.client.post(url, data, format="json")
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(SpacetimeVolume.objects.count(), 2)
+        self.assertEqual(SpacetimeVolume.objects.last().references, ["ref"])
+
+    def test_api_can_update_stv(self):
+        """
+        Ensure we can update SpacetimeVolumes
+        """
+
+        url = reverse("spacetimevolume-detail", args=[self.alsace_stv.pk])
+        data = {
+            "start_date": "0001-01-01",
+            "end_date": "0005-01-01",
+            "entity": self.france.pk,
+            "references": ["ref"],
+            "territory": [self.alsace_geom.pk],
+        }
+        response = self.client.put(url, data, format="json")
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data["end_date"], "0005-01-01")
+
+    def test_api_can_query_stvs(self):
+        """
+        Ensure we can query for all SpacetimeVolumes
+        """
+
+        url = reverse("spacetimevolume-list")
+        response = self.client.get(url, format="json")
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data[0]["end_date"], "0002-01-01")
+
+    def test_api_can_query_stv(self):
+        """
+        Ensure we can query for individual SpacetimeVolumes
+        """
+
+        url = reverse("spacetimevolume-detail", args=[self.alsace_stv.pk])
+        response = self.client.get(url, format="json")
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data["end_date"], "0002-01-01")
