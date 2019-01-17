eeeb638a894561fcda91ca71418d72523e726db2
Liam O Flynn <O Flynn57899>
2019-01-17 10:41:29 -0800

    Remove uniqueness constraint on location

--- project/api/models.py
+++ project/api/models.py
@@ -142,7 +142,7 @@ class City(models.Model):
 
     wikidata_id = models.PositiveIntegerField()  # Excluding the Q
     label = models.TextField(max_length=50)
-    location = models.PointField(unique=True)
+    location = models.PointField()
     inception_date = models.DateField()
     dissolution_date = models.DateField(blank=True, null=True)
 
