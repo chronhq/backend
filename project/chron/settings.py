021e36501c856f99fe4db1b2570cce6ccf8cc886
Liam O'Flynn <lpoflynn@protonmail.ch>
2019-02-06 16:31:54 -0800

    fix settings.py db host

--- project/chron/settings.py
+++ project/chron/settings.py
@@ -95,7 +95,7 @@ DATABASES = {
         "NAME": os.environ.get("POSTGRES_DB", "postgres"),
         "USER": os.environ.get("POSTGRES_USER", "postgres"),
         "PASSWORD": os.environ.get("POSTGRES_PASSWORD", "postgres"),
-        "HOST": "localhost",
+        "HOST": "db",
         "PORT": 5432,
     }
 }
