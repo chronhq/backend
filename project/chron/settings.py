31b8e5d82dd53af539bd06f8342f4b44c1d11da1
whirish <lpoflynn@protonmail.ch>
2019-02-18 11:37:23 -0800

    Comment out default middleware

--- project/chron/settings.py
+++ project/chron/settings.py
@@ -64,14 +64,14 @@ else:
 
 
 MIDDLEWARE = [
-    "django.middleware.security.SecurityMiddleware",
-    "django.contrib.sessions.middleware.SessionMiddleware",
+    #    "django.middleware.security.SecurityMiddleware",
+    #    "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
-    "django.middleware.csrf.CsrfViewMiddleware",
-    "django.contrib.auth.middleware.AuthenticationMiddleware",
-    "django.contrib.auth.middleware.RemoteUserMiddleware",
-    "django.contrib.messages.middleware.MessageMiddleware",
-    "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    #    "django.middleware.csrf.CsrfViewMiddleware",
+    #    "django.contrib.auth.middleware.AuthenticationMiddleware",
+    #    "django.contrib.auth.middleware.RemoteUserMiddleware",
+    #    "django.contrib.messages.middleware.MessageMiddleware",
+    #    "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
 
 if DEBUG:
