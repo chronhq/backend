8c1ae03cd542bd0d7f65ffe857b71aab0e455d85
whirish <lpoflynn@protonmail.ch>
2019-02-20 20:58:18 -0800

    Fix middleware

--- project/chron/settings.py
+++ project/chron/settings.py
@@ -65,12 +65,12 @@ else:
 
 MIDDLEWARE = [
     # "django.middleware.security.SecurityMiddleware",
-    # "django.contrib.sessions.middleware.SessionMiddleware",
+    "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     # "django.middleware.csrf.CsrfViewMiddleware",
-    # "django.contrib.auth.middleware.AuthenticationMiddleware",
+    "django.contrib.auth.middleware.AuthenticationMiddleware",
     # "django.contrib.auth.middleware.RemoteUserMiddleware",
-    # "django.contrib.messages.middleware.MessageMiddleware",
+    "django.contrib.messages.middleware.MessageMiddleware",
     # "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
 
