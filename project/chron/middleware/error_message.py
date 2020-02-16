3b8942b7601ca82d0cd9b65b29fda8e12769054e
Michael Orlov <MiklerGM@gmail.com>
2020-02-16 17:03:52 +0100

    Verbose info on auth errors

--- project/chron/middleware/error_message.py
+++ project/chron/middleware/error_message.py
@@ -1,4 +1,7 @@
 """ Use JSON response for some exceptions """
+import traceback
+import sys
+import re
 from django.utils.deprecation import MiddlewareMixin
 from django.core.exceptions import ValidationError
 from django.http import JsonResponse
@@ -17,5 +20,24 @@ class ErrorMessageFormatter(MiddlewareMixin):  # pylint: disable=too-few-public-
             # https://github.com/garyburgmann/drf-firebase-auth/issues/13
             err = "module 'firebase_admin.auth' has no attribute 'AuthError'"
             if str(exception) == err:
-                return JsonResponse({"error": "Authorization error"}, status=401)
+                t, v, tb = sys.exc_info()  # pylint: disable=invalid-name
+                text = ""
+                for line in reversed(traceback.format_exception(t, v, tb)):
+                    search = re.search("firebase_admin.*\.(.*):", line)
+                    if not search:
+                        continue
+                    # firebase_admin._auth_utils.InvalidIdTokenError
+                    if search.group(1) == "InvalidIdTokenError":
+                        text = "Invalid token. Try to relogin"
+                    # firebase_admin._token_gen.ExpiredIdTokenError
+                    elif search.group(1) == "ExpiredIdTokenError":
+                        text = "Token expired. Try to relogin"
+                    # firebase_admin._token_gen.CertificateFetchError
+                    elif search.group(1) == "CertificateFetchError":
+                        text = "Server can't connect to authentication authority. Try again later"
+                    elif search.group(1) == "RevokedIdTokenError":
+                        text = "Token revoked. Try to relogin"
+                return JsonResponse(
+                    {"error": "Authorization error. {}".format(text)}, status=401
+                )
         return None
