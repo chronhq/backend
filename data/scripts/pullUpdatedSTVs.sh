1e075b4adac0f66f3c27802441bbec5a2d90600e
Michael Orlov <MiklerGM@gmail.com>
2020-03-20 20:47:55 +0100

    Extract SQL (bash) query into separate script

--- data/scripts/pullUpdatedSTVs.sh
+++ data/scripts/pullUpdatedSTVs.sh
@@ -35,28 +35,7 @@ else
     PREV=0
 fi
 
-psql="psql -d \"host=db user=${POSTGRES_USER} password=${POSTGRES_PASSWORD}\" -t -c"
-
-query="
-SELECT string_agg(DISTINCT id::text, ' ') FROM (
-	SELECT api_historicalspacetimevolume.id, api_historicalspacetimevolume.history_date 
-	FROM api_historicalspacetimevolume
-	UNION 
-	SELECT api_spacetimevolume.id, api_historicalterritorialentity.history_date
-	FROM api_spacetimevolume
-	JOIN api_historicalterritorialentity
-	ON api_historicalterritorialentity.id = entity_id
-	JOIN api_territorialentity
-	ON api_territorialentity.id = entity_id
-	WHERE NOT (
-		api_historicalterritorialentity.color_id=api_territorialentity.color_id
-		AND api_historicalterritorialentity.admin_level=api_territorialentity.admin_level
-	)
-) AS foo
-WHERE history_date >= to_timestamp(${PREV})
-"
-
-updates=$(eval $psql "\"$query\"" | sed 's/^\s//')
+updates=$(eval sh ${DIR}/selectUpdatedSTVs.sh)
 
 if [[ -f "${ORIG}" && "${FULL}" == "" ]]; then
     if [[ ${#updates[@]} -eq 0 ]]; then
